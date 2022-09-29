ce53492b97e82b4d951288d9847a5810c6ed600a
aayan <aayan@gmail.com>
2022-09-30 01:08:23 +0530

    clear handle

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,6 +14,12 @@ export default function TextForm(props) {
     setText(newtext);
   }
 
+  const handleClear=()=>{
+    
+    let newtext='';
+    setText(newtext);
+  }
+
   const handleOnChange=(event)=>{
 
     setText(event.target.value)
@@ -26,12 +32,13 @@ export default function TextForm(props) {
  <div className='container'> 
     <h2>{props.heading}</h2> 
         
-    <div className="mb-3">
+     <div className="mb-3">
       <textarea className="form-control"  value={text} onChange={handleOnChange} id="mybox" rows="7" ></textarea>
      </div>
        <button className='btn btn-primary mx-1' onClick={handleUpClick}>Convert to UpperCase</button>
        <button className='btn btn-primary mx-1' onClick={handleDownClick}>Convert to LowerCase</button>
-</div>
+       <button className='btn btn-danger mx-1' onClick={handleClear}>Clear</button>
+     </div>
 <div className='container my-2'>
     <h2>Your text summary</h2>
     <p>{text.split(' ').length} words AND {text.length} characters</p>
